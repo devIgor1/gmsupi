94bccdc7f2edad1bf37dc7c80a4221a12d0ac0a7
Igor <igormraesmm@gmail.com>
2024-01-13 17:37:41 -0300

    feat: Add shadcn-ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,18 @@ import { MdOutlineArrowDropDown } from "react-icons/md"
 const Header = () => {
   return (
     <header className="w-full bg-gradient-to-br from-yellow-900 to-yellow-700">
-      <nav className="wrapper flex-between font-lora bg-gradient-to-br from-slate-50 to-slate-300 bg-clip-text text-transparent">
+      <nav className="wrapper flex-between font-lora text-white">
         <div className="flex items-center">
           <img className="w-[35px] md:w-[50px]" src={pawn} alt="pawn.svg" />
           <h1 className="text-lg md:text-4xl font-bold ">Do Zero a Maestria</h1>
           <img className="w-[35px] md:w-[50px]" src={queen} alt="queen.svg" />
         </div>
-        <div className="text-lg md:text-2xl font-bold flex-between gap-6">
+        <div className="text-lg md:text-xl font-bold flex-between gap-6">
           <a href="#">Inicio</a>
           <div className="flex-between">
             <a href="#">Cursos</a>
             <span>
-              <MdOutlineArrowDropDown size={35} color="#fff" />
+              <MdOutlineArrowDropDown size={30} color="#fff" />
             </span>
           </div>
         </div>
---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,10 @@
+
+
+
+const MobileNav = () => {
+  return(
+
+  )
+}
+
+export default MobileNav
