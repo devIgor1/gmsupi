991b63f00e2cf2dd5d2a5540c0c6e5f5e1832b16
Igor <igormraesmm@gmail.com>
2024-01-13 16:35:28 -0300

    refactor: Remove unnecessary classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        lora: "'Lora', serif",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
