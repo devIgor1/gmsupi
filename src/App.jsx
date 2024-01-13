2901864402d9a5b11d2ea798dece5e75805b7b3c
Igor <igormraesmm@gmail.com>
2024-01-13 16:31:52 -0300

    chore: Initial Configuration + Add header images

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full min-h-screen bg-black">
+        <h1 className="text-white text-4xl font-lora">Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
