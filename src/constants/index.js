fcf4700b0ba58ba2a9c684f55c0f40b0ceec92c2
Igor <igormraesmm@gmail.com>
2024-01-13 18:45:31 -0300

    feat: Add constants

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    label: "Inicio",
+    route: "/",
+  },
+  {
+    label: "Método GM Supi",
+    route: "/metodosupi",
+  },
+  {
+    label: "Método MF Julia 2.0",
+    route: "/metodojulia",
+  },
+  {
+    label: "Aulas ao Vivo",
+    route: "/aulas-ao-vivo",
+  },
+  {
+    label: "Cursos Mensais",
+    route: "/cursos-mensais",
+  },
+]
