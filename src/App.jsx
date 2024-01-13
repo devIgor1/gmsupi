344ecb374b9e3967368d2519759c78c37e103bb2
Igor <igormraesmm@gmail.com>
2024-01-13 16:34:43 -0300

    feat: Add Header as a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
   return (
     <>
-      <div className="w-full min-h-screen bg-black">
-        <h1 className="text-white text-4xl font-lora">Hello</h1>
-      </div>
+      <Header />
     </>
   )
 }
