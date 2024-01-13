930fb403edb6ee9b3343196604ec8af7e45106d8
Igor <igormraesmm@gmail.com>
2024-01-13 19:32:44 -0300

    feat: Add Header and MobileNav layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Header from "./components/Header"
+import Header from "./components/shared/Header"
+import MobileNav from "./components/shared/MobileNav"
 
 function App() {
   return (
     <main className="w-full min-h-screen bg-[#EED6B5]">
-      <Header />
+      <div className="hidden md:block">
+        <Header />
+      </div>
+      <div className="block md:hidden">
+        <MobileNav />
+      </div>
     </main>
   )
 }
