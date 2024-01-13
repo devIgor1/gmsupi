75b3acb871c00fd375d238fb9c7e6b85b1071348
Igor <igormraesmm@gmail.com>
2024-01-13 17:17:42 -0300

    feat: Add react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <>
+    <main className="w-full min-h-screen bg-[#EED6B5]">
       <Header />
-    </>
+    </main>
   )
 }
 
