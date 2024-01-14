56a99c59ee8a299dcee23a422ea0b4b7701fe9c8
Igor <igormraesmm@gmail.com>
2024-01-14 11:21:24 -0300

    feat: Add menubar on Header and customize menubar styles

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,4 +1,18 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
 import { MdOutlineArrowDropDown } from "react-icons/md"
+import { FaChessKing } from "react-icons/fa"
+import { GiChessQueen } from "react-icons/gi"
+import { RiLiveFill } from "react-icons/ri"
+import { IoBookSharp } from "react-icons/io5"
 import Logo from "./Logo"
 
 const Header = () => {
@@ -6,13 +20,48 @@ const Header = () => {
     <header className="w-full bg-gradient-to-br from-yellow-900 to-yellow-700">
       <nav className="wrapper flex-between font-lora text-white">
         <Logo />
-        <div className="text-lg md:text-xl font-bold flex-between gap-6">
-          <a href="#">Inicio</a>
+        <div className="text-lg font-bold flex-between gap-6">
+          <a href="/">Inicio</a>
           <div className="flex-between">
-            <a href="#">Cursos</a>
-            <span>
-              <MdOutlineArrowDropDown size={30} color="#fff" />
-            </span>
+            <Menubar>
+              <MenubarMenu>
+                <MenubarTrigger>
+                  Cursos{" "}
+                  <span>
+                    <MdOutlineArrowDropDown size={30} color="#fff" />
+                  </span>
+                </MenubarTrigger>
+                <MenubarContent className="text-[#7B4610] font-bold">
+                  <MenubarItem className="flex-center gap-3">
+                    <a href="/metodosupi">Método GM Supi</a>
+                    <span>
+                      <FaChessKing />
+                    </span>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem className="flex-center gap-3">
+                    <a href="/metodojulia">Método 2.0 MF Julia</a>
+                    <span>
+                      <GiChessQueen />
+                    </span>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem className="flex-center gap-3">
+                    <a href="/aulasaovivo">Aulas ao Vivo</a>
+                    <span>
+                      <RiLiveFill />
+                    </span>
+                  </MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem className="flex-center gap-3">
+                    <a href="/cursosmensais">Cursos Mensais</a>
+                    <span>
+                      <IoBookSharp />
+                    </span>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+            </Menubar>
           </div>
         </div>
       </nav>
--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -3,7 +3,7 @@ import queen from "../../assets/images/queen.svg"
 
 const Logo = () => {
   return (
-    <div className="flex items-center text-white font-lora">
+    <div className="flex items-center bg-gradient-to-r from-slate-200 to-slate-100 bg-clip-text text-transparent font-lora">
       <img className="w-[35px] md:w-[50px]" src={pawn} alt="pawn.svg" />
       <h1 className="text-2xl md:text-4xl font-bold">Do Zero a Maestria</h1>
       <img className="w-[35px] md:w-[50px]" src={queen} alt="queen.svg" />
