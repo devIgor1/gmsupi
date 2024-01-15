5618d50b23bdbc8a7d8fd351d86f6cdc26f18af2
Igor <igormraesmm@gmail.com>
2024-01-15 08:54:37 -0300

    refactor: Turn button into a component

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,9 @@
+const Button = (className) => {
+  return (
+    <button className="bg-[#B81919] text-white text-base md:text-2xl p-2 px-7 md:p-4 md:px-10 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font-semibold rounded-lg">
+      PRÓXIMA TURMA 05/02
+    </button>
+  )
+}
+
+export default Button
