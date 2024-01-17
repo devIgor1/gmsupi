8231368f687ff56656735cb6bb33c8fb15aeca85
Igor <igormraesmm@gmail.com>
2024-01-17 11:10:25 -0300

    style: Change button font size

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (className) => {
   return (
-    <button className="bg-[#B81919] text-white text-base md:text-2xl p-2 px-7 md:p-4 md:px-10 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font-semibold rounded-lg">
+    <button className="bg-[#B81919] text-white text-base md:text-xl p-2 px-7 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font- rounded-lg">
       PRÓXIMA TURMA 05/02
     </button>
   )
