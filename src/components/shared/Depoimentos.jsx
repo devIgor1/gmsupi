c23c98311d5c19f87e531edb07f5ec5fcbb129c7
Igor <igormraesmm@gmail.com>
2024-01-17 09:54:31 -0300

    feat: Finish "depoimentos" section

--- src/components/shared/Depoimentos.jsx
+++ src/components/shared/Depoimentos.jsx
@@ -2,7 +2,114 @@ const Depoimentos = () => {
   return (
     <div className="mt-20 min-h-screen">
       <div className="flex-center">
-        <h1 className="text-5xl font-lora">Alguns Depoimentos</h1>
+        <h1 className="text-5xl font-lora underline text-center">
+          Alguns Depoimentos
+        </h1>
+      </div>
+      <div className="flex-center mt-10 bg-black wrapper rounded-lg">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5 px-10 py-5">
+          <div>
+            <img
+              src="/src/assets/images/depoimentos/depoimento19.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center flex-col">
+            <img
+              src="/src/assets/images/depoimentos/depoimento13.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento16.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento17.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center flex-col">
+            <img
+              src="/src/assets/images/depoimentos/depoimento15.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento14.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center flex-col">
+            <img
+              src="/src/assets/images/depoimentos/depoimento4.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento18.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center">
+            <img
+              src="/src/assets/images/depoimentos/depoimento5.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center flex-col">
+            <img
+              src="/src/assets/images/depoimentos/depoimento2.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento11.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento9.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center">
+            <img
+              src="/src/assets/images/depoimentos/depoimento12.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center">
+            <img
+              src="/src/assets/images/depoimentos/depoimento7.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center">
+            <img
+              src="/src/assets/images/depoimentos/depoimento8.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center">
+            <img
+              src="/src/assets/images/depoimentos/depoimento6.jpg"
+              alt="depoimento"
+            />
+          </div>
+          <div className="flex-center">
+            <img
+              src="/src/assets/images/depoimentos/depoimento10.jpg"
+              alt="depoimento"
+            />
+          </div>
+
+          <div className="flex-center flex-col">
+            <img
+              src="/src/assets/images/depoimentos/depoimento3.jpg"
+              alt="depoimento"
+            />
+            <img
+              src="/src/assets/images/depoimentos/depoimento1.jpg"
+              alt="depoimento"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
