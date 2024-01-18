c6eb21d4923062c6d772be015a5f3e3a6d6241f1
Igor <igormraesmm@gmail.com>
2024-01-18 09:12:25 -0300

    style: Change background color

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -7,6 +7,7 @@ import Vantagens from "@/components/shared/Vantagens"
 import Depoimentos from "@/components/shared/Depoimentos"
 import Price from "@/components/shared/Price"
 import Perguntas from "@/components/shared/Perguntas"
+import Footer from "@/components/shared/Footer"
 
 export default function Home() {
   return (
@@ -17,7 +18,7 @@ export default function Home() {
       <div className="block md:hidden">
         <MobileNav />
       </div>
-      <main className="w-full min-h-screen bg-gradient-to-t from-orange-300 to-orange-200">
+      <main className="w-full min-h-screen bg-[#0D0D0D]">
         <Banner />
         <AssinaturaAnual />
         <Vantagens />
@@ -25,6 +26,7 @@ export default function Home() {
         <Price />
         <Perguntas />
       </main>
+      <Footer />
     </>
   )
 }
