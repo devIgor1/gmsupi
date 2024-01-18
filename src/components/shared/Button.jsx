6be5e11ded87c28a825ba0afe253216085b6aae9
Igor <igormraesmm@gmail.com>
2024-01-18 08:55:37 -0300

    style: Change button color

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (className) => {
   return (
-    <button className="bg-[#B81919] text-white text-base md:text-xl p-2 px-7 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font- rounded-lg">
+    <button className="bg-[#A61420] text-white text-base md:text-xl p-2 px-7 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font- rounded-lg">
       PRÓXIMA TURMA 05/02
     </button>
   )
