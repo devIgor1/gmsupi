744e568c66e3b54c88a012b5be3106e55ad8dae3
Igor <igormraesmm@gmail.com>
2024-01-18 17:22:33 -0300

    fix: Fix logo size

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -3,9 +3,11 @@ import queen from "/assets/images/queen.svg"
 
 const Logo = () => {
   return (
-    <a href="/" className="flex items-center text-[#F2EFDF] font-dmSerif">
+    <a href="/" className="flex-center text-[#F2EFDF] font-dmSerif">
       <img className="w-[35px] md:w-[50px]" src={pawn} alt="pawn.svg" />
-      <h1 className="text-2xl md:text-4xl font-bold">Do Zero à Maestria</h1>
+      <h1 className="text-2xl md:text-4xl font-bold text-center">
+        Do Zero à Maestria
+      </h1>
       <img className="w-[35px] md:w-[50px]" src={queen} alt="queen.svg" />
     </a>
   )
