8655c394ef911f5b8ffbf8e1cecce9a2709610d4
Igor <igormraesmm@gmail.com>
2024-01-18 12:25:00 -0300

    feat: Finish "metodo supi" page

--- src/pages/metodosupi/index.jsx
+++ src/pages/metodosupi/index.jsx
@@ -1,17 +1,187 @@
 import Header from "@/components/shared/Header"
 import MobileNav from "@/components/shared/MobileNav"
+import { Separator } from "@/components/ui/separator"
+import { FaYoutube } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
+import { FaFacebook } from "react-icons/fa"
 
 export default function MetodoSupi() {
   return (
     <nav>
-      <div className="hidden md:block">
+      <div className="hidden md:block border-b-2 border-[#F2EFDF]">
         <Header />
       </div>
       <div className="block md:hidden">
         <MobileNav />
       </div>
-      <main className="min-h-screen w-full bg-gradient-to-b from-orange-100 to-orange-200">
-        <h1>Hello Main</h1>
+      <main className="min-h-screen w-full bg-[#0D0D0D]">
+        <section className="wrapper pt-5 w-full flex-around">
+          <div className="bg-[#F2EFDF] w-full h-full md:h-96 flex flex-col md:flex-row md:flex-between rounded-md gap-5 p-5 md:p-0">
+            <div className="text-start flex-1 md:ml-20 flex-col">
+              <h1 className="font-dmSerif text-4xl md:text-5xl mb-5 underline">
+                MÉTODO GM SUPI
+              </h1>
+              <p className="font-merriweather font-bold text-xl text-[#A61420]">
+                GM Luis Paulo Supi
+              </p>
+              <p className="font-merriweather mt-5 w-[300px] md:w-full">
+                Método de Treinamento de um dos principais nomes do Xadrez
+                Brasileiro na atualidade. São mais de 40 aulas contando a
+                trajetória de quando o{" "}
+                <span className="text-[#A61420] font-bold">GM</span> Supi
+                iniciou no Xadrez, até ultrapassar a barreira dos 2600.
+              </p>
+              <div className="flex items-center justify-start mt-5">
+                <div className="flex-center gap-10">
+                  <button className="bg-black text-[#F2EFDF] font-dmSerif text-xl py-1 px-5 rounded-md hover:scale-105 duration-300">
+                    Sobre
+                  </button>
+                  <div className="flex-center gap-5">
+                    <a href="#" className="hover:scale-110 duration-300">
+                      <FaYoutube size={30} />
+                    </a>
+                    <a href="#" className="hover:scale-110 duration-300">
+                      <FaInstagram size={30} />
+                    </a>
+                    <a href="#" className="hover:scale-110 duration-300">
+                      <FaFacebook size={30} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <img
+                src="/src/assets/images/metodos/supimetodo.jpg"
+                alt="supi"
+                className=" md:h-[384px] rounded-md md:rounded-tl-none md:rounded-bl-none md:rounded-tr-md md:rounded-br-md"
+              />
+            </div>
+          </div>
+        </section>
+        <section className="min-h-screen mt-10 flex-center">
+          <div className="bg-[#F2EFDF] grid grid-cols-1 md:grid-cols-3 p-5 gap-5 rounded-md">
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5 font-bold">
+                MÓDULO 1: INSTRUÇÕES GERAIS
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                INSTRUÇÕES GERAIS
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5 font-bold">
+                MÓDULO 2: INICIANTE RATING 0 - 1900
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                0 A 1000 DE RATING
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5 font-bold">
+                MÓDULO 3: RATING 1900 - 2200
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                RATING 1900 A 2000
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5">
+                MÓDULO 4: RATING 2200 - 2300
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                RATING 2200 A 2300
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5">
+                MÓDULO 5: MESTRE INTERNACIONAL
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                MESTRE INTERNACIONAL
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5">
+                MÓDULO 6: GRANDE MESTRE
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                GRANDE MESTRE
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+            <div className="bg-[#0D0D0D] flex-center flex-col rounded-md max-w-[500px]">
+              <h1 className="text-[#F2EFDF] text-center text-3xl font-dmSerif p-5">
+                MÓDULO 7 BÔNUS: 2500 A NÚMERO 1 BR
+              </h1>
+              <Separator className="bg-[#F2EFDF]" />
+              <img
+                src="/src/assets/images/chess.png"
+                alt="chess"
+                className="relative"
+              />
+              <span className="relative bg-[#F2EFDF] text-[#0D0D0D] font-merriweather -top-36 p-5 text-2xl rounded-lg">
+                2500 A NÚMERO 1 BR
+              </span>
+              <button className="border border-[#F2EFDF] mb-5 text-[#F2EFDF] text-2xl p-3 font-merriweather rounded-md hover:scale-105 duration-300">
+                ACESSO AO CONTEÚDO
+              </button>
+            </div>
+          </div>
+        </section>
       </main>
     </nav>
   )
