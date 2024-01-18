4b6b7d9b7fccee85b2632c8f88989d3d0cc2f09f
Igor <igormraesmm@gmail.com>
2024-01-18 17:12:22 -0300

    style: Items-center

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-black text-white flex-center h-11 font-poppins">
+    <div className="bg-black text-white flex-center h-11 font-poppins text-center">
       <h1>© 2023 by GM Luis Paulo Supi & MF Julia Alboredo</h1>
     </div>
   )
--- src/components/shared/MobileNav.jsx
+++ src/components/shared/MobileNav.jsx
@@ -18,7 +18,7 @@ const MobileNav = () => {
   return (
     <header className="w-full bg-[#0D0D0D] border-b-2 border-[#F2EFDF]">
       <nav className="wrapper flex-between font-dmSerif text-[#F2EFDF]">
-        <a href="/" className="flex items-center">
+        <a href="/" className="flex-center">
           <img className="w-[35px]" src={pawn} alt="pawn.svg" />
           <h1 className="text-2xl font-bold ">Do Zero a Maestria</h1>
           <img className="w-[35px]" src={queen} alt="queen.svg" />
