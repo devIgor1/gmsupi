e7d0f9c19edc983e4533ccc78c22ddb13012cb92
Igor <igormraesmm@gmail.com>
2024-01-18 08:55:27 -0300

    style: Change color palette and font

--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -5,11 +5,11 @@ const Banner = () => {
     <>
       <section className="flex-center">
         <div className="bg-zinc-900/75 md:bg-zinc-900/65 absolute w-full h-[250px] md:h-[578px]">
-          <div className="text-2xl md:text-6xl lg:text-7xl text-white md:max-w-[800px] mx-auto mt-5 font-medium font-lora">
+          <div className="text-2xl md:text-6xl lg:text-7xl text-[#F2EFDF] md:max-w-[800px] mx-auto mt-5 font-medium font-dmSerif">
             <div className="bg-black/80 rounded-lg p-1">
               <h1 className="text-center">
-                Do <span className="text-[#B81919]">Zero</span> à{" "}
-                <span className="text-green-500">Maestria</span> com os Melhores
+                Do <span className="text-[#A61420]">Zero</span> à{" "}
+                <span className="text-[#10611D]">Maestria</span> com os Melhores
                 Enxadristas do Brasil
               </h1>
             </div>
