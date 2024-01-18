3591d11c919c6d54faad75d3da62bf690a7f14d1
Igor <igormraesmm@gmail.com>
2024-01-18 08:57:38 -0300

    style: Change font color

--- src/components/shared/Logo.jsx
+++ src/components/shared/Logo.jsx
@@ -3,7 +3,7 @@ import queen from "../../assets/images/queen.svg"
 
 const Logo = () => {
   return (
-    <div className="flex items-center bg-gradient-to-r from-slate-200 to-slate-100 bg-clip-text text-transparent font-lora">
+    <div className="flex items-center text-[#F2EFDF] font-dmSerif">
       <img className="w-[35px] md:w-[50px]" src={pawn} alt="pawn.svg" />
       <h1 className="text-2xl md:text-4xl font-bold">Do Zero à Maestria</h1>
       <img className="w-[35px] md:w-[50px]" src={queen} alt="queen.svg" />
