df2da222a7da942b8e0bad121b467a05951c2259
Igor <igormraesmm@gmail.com>
2024-01-18 17:15:26 -0300

    style: Change opacity

--- src/components/shared/Banner.jsx
+++ src/components/shared/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <>
       <section className="flex-center">
-        <div className="bg-zinc-900/75 md:bg-zinc-900/65 absolute w-full h-[250px] md:h-[578px]">
+        <div className="bg-zinc-900/75 md:bg-zinc-900/50 absolute w-full h-[250px] md:h-[578px]">
           <div className="text-2xl md:text-6xl lg:text-7xl text-[#F2EFDF] md:max-w-[800px] mx-auto mt-5 font-medium font-dmSerif">
             <div className="bg-black/80 rounded-lg p-1">
               <h1 className="text-center">
