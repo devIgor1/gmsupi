829fe9a21317de64fdc1ba00778d214eb5edbeb5
Igor <igormraesmm@gmail.com>
2024-01-18 09:15:46 -0300

    style: Change width on mobile devices

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (className) => {
   return (
-    <button className="bg-[#A61420] text-white text-base md:text-xl p-2 px-7 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font-dmSerif rounded-md">
+    <button className="bg-[#A61420] text-white text-lg md:text-xl p-2 px-7 border-double border-8 border-black shadow-2xl shadow-[#B81919] hover:scale-105 duration-300 font-dmSerif rounded-md">
       PRÓXIMA TURMA 05/02
     </button>
   )
