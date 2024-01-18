07fac58d0f64c522f5c67df010038ac39759457a
Igor <igormraesmm@gmail.com>
2024-01-18 09:12:05 -0300

    style: Change color palette

--- src/components/shared/Perguntas.jsx
+++ src/components/shared/Perguntas.jsx
@@ -8,34 +8,34 @@ import { BsQuestionCircleFill } from "react-icons/bs"
 
 const Perguntas = () => {
   return (
-    <div className="mt-20 min-h-screen">
-      <div className="max-w-4xl flex-center flex-col bg-black mx-auto p-5 text-white font-poppins gap-5 rounded-lg shadow-lg shadow-black border-2 border-white">
+    <div className="mt-20 pb-10">
+      <div className="max-w-4xl flex-center flex-col mx-auto p-5 text-white font-merriweather gap-5 rounded-md border-2 border-[#F2EFDF]">
         <div className="flex-center gap-3 mb-10">
           <span>
-            <BsQuestionCircleFill size={50} />
+            <BsQuestionCircleFill size={50} color="#F2EFDF" />
           </span>
-          <h1 className="text-3xl md:text-5xl font-lora underline text-center">
+          <h1 className="text-3xl md:text-5xl font-dmSerif underline text-center text-[#F2EFDF]">
             PERGUNTAS FREQUENTES
           </h1>
           <span>
-            <BsQuestionCircleFill size={50} />
+            <BsQuestionCircleFill size={50} color="#F2EFDF" />
           </span>
         </div>
-        <Accordion type="single" collapsible className="w-full">
+        <Accordion type="single" collapsible className="w-full text-xl">
           <AccordionItem value="item-1">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Por quanto tempo vou ter acesso aos cursos?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Você terá acesso aos cursos durante o período de 12 meses e poderá
               renovar a assinatura pelo mesmo valor.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Como vou receber o produto após realizar a compra?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Assim que o pagamento for aprovado através da Hotmart, você
               receberá um e-mail com as credenciais para ter acesso aos cursos
               nesta plataforma. Caso tenha qualquer dúvida, entrar em contato:
@@ -45,10 +45,10 @@ const Perguntas = () => {
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Como saber se estes cursos são para o meu nível?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Estes cursos foram estruturados para ajudar jogadores de TODOS os
               níveis no xadrez, mesmo aqueles que ainda não sabem mexer as peças
               ou nunca tiveram contato com o jogo. Eles abordam desde os
@@ -59,10 +59,10 @@ const Perguntas = () => {
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-4">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Eu vou ter contato com os professores durante o curso?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Sim! Os 300 primeiros alunos terão acesso direto aos professores
               através de um grupo exclusivo de WhatsApp durante o período de
               assinatura (12 meses). Nele, você poderá interagir com os
@@ -71,40 +71,40 @@ const Perguntas = () => {
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-5">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               O conteúdo é ao vivo ou gravado?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Você receberá todo o conteúdo dos cursos gravado com altíssima
               qualidade. Além disso, poderá participar das aulas ao vivo toda
               semana com nossa equipe de professores.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-6">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Estes cursos terão atualizações?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Estes cursos foram desenvolvidos da maneira mais completa
               possível, mas todos os meses serão disponibilizados cursos novos
               sem nenhum custo adicional aos Alunos.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-7">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Estes cursos serão por este preço para sempre?
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Os alunos desta turma, terão acesso a um desconto em sua
               assinatura. Faça sua matrícula e garanta este preço exclusivo,
               pois a qualquer momento o preço pode voltar a ser o original.
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-8">
-            <AccordionTrigger>
+            <AccordionTrigger className="font-bold text-[#F2EFDF]">
               Quais as formas de pagamento disponíveis?{" "}
             </AccordionTrigger>
-            <AccordionContent className="text-green-500">
+            <AccordionContent className="text-lg text-[#F2F2F2]">
               Você poderá efetuar o pagamento por cartão, pix ou boleto
               bancário. Para outras formas de pagamento, entre em contato com
               <span className="underline ml-1">
